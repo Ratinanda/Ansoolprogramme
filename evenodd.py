07461ac0420551e3d32bfdc05fe0fc253ca8cef9
Ratinanda <40784541+Ratinanda@users.noreply.github.com>
2020-12-25 23:30:00 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,7 @@
+print("Welcome to python")
+print("This is a game about Even and ODD Number")
+num1=int(input("enter the number:"))
+if num1%2 == 0:
+    print("The number "+str(num1)+" you entered is a even number")
+else:
+    print("The number "+str(num1)+" you entered is a odd number")
